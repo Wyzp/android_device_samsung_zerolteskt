a859e45a06772ef59f0f1e67e519adbe98c70bf1
Lukas0610 <mail@lukasberger.at>
2017-12-07 12:03:59 +0100

    zerolteskt: added product-configuration for NexusOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,4 +18,5 @@ add_lunch_combo aokp_zerolteskt-userdebug
 add_lunch_combo aosp_zerolteskt-userdebug
 add_lunch_combo gzosp_zerolteskt-userdebug
 add_lunch_combo lineage_zerolteskt-userdebug
+add_lunch_combo nexus_zerolteskt-userdebug
 add_lunch_combo omni_zerolteskt-userdebug
